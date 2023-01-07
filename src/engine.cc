09fbfa6ca673ea34aacb6c35edb6898036093ac9
Xavier Gomez <xavier.gomez@epita.fr>
2023-01-07 15:17:45 +0100

    [FIX] vulkan build + added window construction

--- src/engine.cc
+++ src/engine.cc
@@ -1,142 +1,39 @@
-#include <dlfcn.h>
+#define GLFW_INCLUDE_VULKAN
+#include <GLFW/glfw3.h>
+
 #include <iostream>
-#include <vector>
+#include <stdexcept>
+#include <cstdlib>
 
-#include "my_asserts.hh"
-#include "VulkanFunctions.h"
+const uint32_t WIDTH = 800;
+const uint32_t HEIGHT = 600;
 
+GLFWwindow* window;
+
+void initWindow() {
+    glfwInit();
+    glfwWindowHint(GLFW_CLIENT_API, GLFW_NO_API);
+    glfwWindowHint(GLFW_RESIZABLE, GLFW_FALSE);
+    window = glfwCreateWindow(WIDTH, HEIGHT, "Vulkan Engine", nullptr, nullptr);
+}
+void initVulkan() {
+}
+
+void mainLoop() {
+    while (!glfwWindowShouldClose(window)) {
+        glfwPollEvents();
+    }
+}
+void cleanup() {
+    glfwDestroyWindow(window);
+    glfwTerminate();
+}
 
 int main()
 {
-  void* vulkan_library = dlopen("libvulkan.so.1", RTLD_NOW);
+  initWindow();
+  mainLoop();
+  cleanup();
 
-  if (!vulkan_library)
-    std::cout << "Could not connect with Vulkan library." << std::endl;
-
-
-  #define INSTANCE_LEVEL_VULKAN_FUNCTION( name )                          \
-    name = (PFN_##name)vkGetInstanceProcAddr( instance, #name );          \
-    if( name == nullptr ) {                                               \
-      std::cout << "Could not load instance-level Vulkan function named: "\
-      #name << std::endl;                                                 \
-      return 1;                                                           \
-    }
-
-  #define INSTANCE_LEVEL_VULKAN_FUNCTION_FROM_EXTENSION( name, extension )     \
-    for( auto & enabled_extension : enabled_extensions ) {                     \
-      if( std::string( enabled_extension ) == std::string( extension ) ) {     \
-        name = (PFN_##name)vkGetInstanceProcAddr( instance, #name );           \
-        if( name == nullptr ) {                                                \
-          std::cout << "Could not load instance-level Vulkan function named: " \
-          #name << std::endl;                                                  \
-          return false;                                                        \
-        }                                                                      \
-      }                                                                        \
-    }
-
-  #include "ListOfVulkanFunctions.inl"
-  PFN_vkEnumerateInstanceExtensionProperties vkEnumerateInstanceExtensionProperties;
-  PFN_vkEnumerateInstanceLayerProperties vkEnumerateInstanceLayerProperties;
-  PFN_vkCreateInstance vkCreateInstance;
-
-  //
-  //  Loading global level-functions
-  //
-
-  //Use to check available instance-level extension
-  vkEnumerateInstanceExtensionProperties =  (PFN_vkEnumerateInstanceExtensionProperties) vkGetInstanceProcAddr(nullptr, "vkEnumerateInstanceExtensionProperties");
-  //Use to check available layer
-  vkEnumerateInstanceLayerProperties = (PFN_vkEnumerateInstanceLayerProperties) vkGetInstanceProcAddr(nullptr, "vkEnumerateInstanceLayerProperties");
-  //Use to create instance
-  vkCreateInstance = (PFN_vkCreateInstance) vkGetInstanceProcAddr(nullptr, "vkCreateInstance");
-
-  CHK_ELSE_RET(vkEnumerateDeviceExtensionProperties);
-  CHK_ELSE_RET(vkEnumerateInstanceExtensionProperties);
-  CHK_ELSE_RET(vkCreateInstance);
-
-  //
-  //  Checking available instance extensions
-  //
-
-  uint32_t nNbExtensions = 0;
-  CHK_VK_SUCCESS(vkEnumerateInstanceExtensionProperties(nullptr, &nNbExtensions, nullptr));
-  std::cout << nNbExtensions <<  ": Available instance-level extension." << std::endl;
-
-  std::vector<VkExtensionProperties> vAvailableExtensions(nNbExtensions);
-  CHK_VK_SUCCESS(vkEnumerateInstanceExtensionProperties(nullptr, &nNbExtensions, vAvailableExtensions.data()));
-
-  //
-  //  Create Vulkan instance
-  //
-
-  std::vector<char const *> vDesiredExtension;
-  VkApplicationInfo vkApplicationInfo = {
-    .sType = VK_STRUCTURE_TYPE_APPLICATION_INFO,
-    .pNext = nullptr,
-    .pApplicationName = "Vulkan Engine",
-    .applicationVersion = VK_MAKE_VERSION(1, 0, 0),
-    .pEngineName = "Boo",
-    .engineVersion = VK_MAKE_VERSION(1, 0, 0),
-    .apiVersion = VK_MAKE_VERSION(1, 0, 0)
-  };
-
-  VkInstanceCreateInfo vkInstanceCreateInfo = {
-    .sType = VK_STRUCTURE_TYPE_INSTANCE_CREATE_INFO,
-    .pNext = nullptr,
-    .flags = 0,
-    .pApplicationInfo = &vkApplicationInfo,
-    .enabledLayerCount = 0,
-    .ppEnabledLayerNames = nullptr,
-    .enabledExtensionCount = vDesiredExtension.size(),
-    .ppEnabledExtensionNames = vDesiredExtension.size() ? vDesiredExtension.data() : nullptr
-  };
-
-  VkInstance vkInstance;
-  CHK_VK_SUCCESS(vkCreateInstance(&vkInstanceCreateInfo, nullptr, &vkInstance));
-
-  //
-  //Loading instance-level functions
-  //
-
-
-  //
-  //  Enumerate availale physical devices
-  //
-
-  uint32_t nNbDevices = 0;
-  CHK_VK_SUCCESS(vkEnumeratePhysicalDevices(vkInstance, &nNbDevices, nullptr));
-
-  std::vector<VkPhysicalDevice> vAvailableDevices(nNbDevices);
-  CHK_VK_SUCCESS(vkEnumeratePhysicalDevices(vkInstance, &nNbDevices, vAvailableDevices.data()));
-  std::cout << nNbDevices << ": Available devices." << std::endl;
-
-  if (!nNbDevices)  return 1;
-
-  //
-  //  Checking available device extensions
-  //
-
-  VkPhysicalDevice vkPhysicalDevice = vAvailableDevices[0];
-  uint32_t nNbDeviceExtensions = 0;
-  CHK_VK_SUCCESS(vkEnumerateDeviceExtensionProperties(vkPhysicalDevice, nullptr, &nNbDeviceExtensions, nullptr));
-
-  std::vector<VkExtensionProperties> vExtensionProperties(nNbDeviceExtensions);
-  CHK_VK_SUCCESS(vkEnumerateDeviceExtensionProperties(vkPhysicalDevice, nullptr, &nNbDeviceExtensions, vExtensionProperties.data()));
-
-  //
-  //  Getting features and properties of a physical device
-  //
-
-  VkPhysicalDeviceFeatures vkDeviceFeatures;
-  VkPhysicalDeviceProperties vkDeviceProperties;
-
-  vkGetPhysicalDeviceFeatures(vkPhysicalDevice, &vkDeviceFeatures);
-  vkGetPhysicalDeviceProperties(vkPhysicalDevice, &vkDeviceProperties);
-
-  std::cout << "Device Name: " << vkDeviceProperties.deviceName << std::endl;
-  std::cout << "Device Type: " << vkDeviceProperties.deviceType << std::endl;
-
-  std::cout << "Program ended successfully !" << std::endl;
-
-  return 0;
+  return EXIT_SUCCESS;
 }
